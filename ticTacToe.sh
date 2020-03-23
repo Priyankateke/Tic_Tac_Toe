51b2b2425bee7691e54afc680677acb315d7cdff
Priyankateke <priiyankateke1705@gmail.com>
2020-03-23 13:37:24 -0700

    ADDED: Checking Row/Column/Diagonal For Winning & Tie Condintions

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -33,13 +33,18 @@ function displayBoard()
 function tossForPlay()
 {
 	if [ $(( RANDOM % 2 )) -eq 0 ]; then
-		player='X'
-		playerTurn=true
+		player=X
 	else
-		player='O'
-		playerTurn=true
+		player=O
 	fi
 	echo "player sign $player"
+}
+
+#Switching sign assign to players
+function switchPlayerSign()
+{
+	#Checking condition using Ternary operators
+	[ $player == X ] && player=O || player=X
 }
 
 #Function for user play
@@ -54,6 +59,19 @@ function userPlay()
 	fi
 }
 
+#checking position is already filled or blank
+function isCellEmpty() {
+	local position=$1-1
+	if((${gameBoard[position]}!=X && ${gameBoard[position]}!=O))
+	then
+		gameBoard[$position]=$player
+		((playerMoves++))
+	else
+		echo "Position is Occupied"
+		userPlay
+	fi
+}
+
 #Running game untill game ends
 function playTillGameEnd()
 {
@@ -61,18 +79,33 @@ function playTillGameEnd()
 	do
 		userPlay
 		displayBoard
+		checkWinningCells
+		switchPlayerSign
 	done
+	echo "Game Tie"
 }
 
-#checking position is already filled or blank
-function isCellEmpty() {
-	local position=$1-1
-	if((${gameBoard[position]}!=$player))
-	then
-		gameBoard[$position]=$player
-		((playerMoves++))
-	else
-		echo "Position is Occupied"
+#Checking column, rows and diagonals
+function checkWinningCells()
+{
+	col=0
+	for((row=0;row<7;row+=3))
+	do
+		checkWinner ${gameBoard[$row]} ${gameBoard[$((row+1))]} ${gameBoard[$((row+2))]}
+		checkWinner ${gameBoard[$col]} ${gameBoard[$((col+3))]} ${gameBoard[$((col+6))]}
+		((col++))
+	done
+	checkWinner ${gameBoard[0]} ${gameBoard[4]} ${gameBoard[8]}
+	checkWinner ${gameBoard[2]} ${gameBoard[4]} ${gameBoard[6]}
+}
+
+#Checking winner
+function checkWinner()
+{
+	local cell1=$1 cell2=$2 cell3=$3
+	if [ $cell1 == $cell2 ] && [ $cell2 == $cell3 ]; then
+		echo "Player Win and have sign $player"
+		exit
 	fi
 }
 
