fef172019491149596c0cd0f50fcd7e5597ba8dc
Priyankateke <priiyankateke1705@gmail.com>
2020-03-23 05:34:23 -0700

    ADDED: Restting Game Board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome To Tic Tac Toe"
+
+#array for game board
+declare -a gameBoard
+
+#Restting game board
+function resetBoard()
+{
+	gameBoard=(- - - - - - - - -)
+}
+
